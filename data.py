60a01cc583af336c44ed9476649ab86c23e2e212
Magnus Torvund <magnus.torvund@gmail.com>
2021-08-27 16:59:57 +0200

    Added functions

---
+++ data.py
@@ -0,0 +1,76 @@
+"""
+This python file formats the data from db.py
+"""
+import pandas as pd
+from db import get_data, get_gameweek_history, get_season_history
+from tqdm import tqdm
+tqdm.pandas()
+
+  
+def create_player_df():
+    """
+    Create a baseline player dataframe for all players
+
+    Args:
+        player_id: The player_id of the player
+
+    Returns:
+        A dataframe containing gameweek stats for each gameweek in the current season
+    """
+    
+    # retrieve data
+    data = get_data()
+    players = data[0]
+    teams = data[1]
+    positions = data[2]
+    
+    # select columns of interest from players df
+    players = players[
+        ['id', 'first_name', 'second_name', 'web_name', 'team',
+        'element_type']
+    ]
+
+    # join team name
+    players = players.merge(
+        teams[['id', 'name']],
+        left_on='team',
+        right_on='id',
+        suffixes=['_player', None]
+    ).drop(['team', 'id'], axis=1)
+
+    # join player positions
+    players = players.merge(
+        positions[['id', 'singular_name_short']],
+        left_on='element_type',
+        right_on='id'
+    ).drop(['element_type', 'id'], axis=1)
+    
+    return players
+
+
+def get_full_gw_hist(players_df: pd.DataFrame):
+    """
+    Get a gameweek stat history for all registred players
+
+    Args:
+        players_df (pd.Dataframe): A DataFrame containing id's for PL players
+
+    Returns:
+        A dataframe containing gameweek stats for all players in the input.
+    """
+    players = players_df
+    
+    # get gameweek histories for each player
+    points = players['id_player'].progress_apply(get_gameweek_history)
+
+    # combine results into single dataframe
+    points = pd.concat(df for df in points)
+
+    # join web_name
+    points = players[['id_player', 'web_name']].merge(
+        points,
+        left_on='id_player',
+        right_on='element'
+    )
+    
+    return points
--- db.py
+++ db.py
@@ -1,6 +1,69 @@
-    """
+"""
     This python file connects to endpoints in the fantasy premier league
     and convert the json data into dataframes
+"""
+    
+import requests, json
+import pandas as pd
+
+# base url
+BASE_URL = 'https://fantasy.premierleague.com/api/'
+
+def get_data():
     """
+    Retrieve data on players, teams and positions from fantasy PL from a static endpoint
+
+    Returns:
+        Dataframe for players, teams and positions
+    """
+    # the specific request endpoint
+    r = requests.get(BASE_URL + 'bootstrap-static/').json()
     
+    # get player information
+    players = pd.json_normalize(r['elements'])
+    # get team information
+    teams = pd.json_normalize(r['teams'])
+    # get position information
+    positions = pd.json_normalize(r['element_types'])
+
+    return players, teams, positions
+
+def get_gameweek_history(player_id):
+    """
+    Get gameweek history for a given player_id (PID) in current season
+
+    Args:
+        player_id: The player_id of the player
+
+    Returns:
+        A dataframe containing gameweek stats for each gameweek in the current season
+    """
+    # send GET request to
+    # https://fantasy.premierleague.com/api/element-summary/{PID}/
+    gw_r = requests.get(BASE_URL + 'element-summary/' + str(player_id) + '/').json()
+    # extract 'history' data from response into dataframe
+    df = pd.json_normalize(gw_r['history'])
     
+    return df
+
+def get_season_history(player_id):
+    """
+    Get stats from previous seasons for a given player_id (PID)
+
+    Args:
+        player_id: The player_id of the player
+
+    Returns:
+        A dataframe containing stats for each season for the specified player
+    """
+ 
+    # send GET request to
+    # https://fantasy.premierleague.com/api/element-summary/{PID}/
+    r = requests.get(
+            BASE_URL + 'element-summary/' + str(player_id) + '/'
+    ).json()
+    
+    # extract 'history_past' data from response into dataframe
+    df = pd.json_normalize(r['history_past'])
+    
+    return df
